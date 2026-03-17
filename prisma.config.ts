aed53c2e513545193589c3c4ef7fe6d0c143c82c
timoeyouwayz <timoeyouwayz@gmail.com>
2026-03-17 17:03:08 +0300

    Fix prisma.config.ts with single line URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,7 @@
 const config = {
   datasources: {
     db: {
-      url:
-        process.env.DATABASE_URL ||
-        process.env.DATABASE_POSTGRES_URL ||
-        "postgresql://neondb_owner:npg_cRqfN7iK1hpv@ep-late-mountain-ad4c2oke-pooler.c-2.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
+      url: process.env.DATABASE_URL || process.env.DATABASE_POSTGRES_URL || "postgresql://neondb_owner:npg_cRqfN7iK1hpv@ep-late-mountain-ad4c2oke-pooler.c-2.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"
     },
   },
 };
